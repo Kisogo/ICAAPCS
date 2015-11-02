39a38d79ba2ef724effeacb662f574aac188dd72
Ryan Stenmark <ryanpstenmark@gmail.com>
2015-11-02 17:08:05 -0600

    commit

--- 03 Unit/03 Lesson/CharDecoder.java
+++ 03 Unit/03 Lesson/CharDecoder.java
@@ -2,14 +2,11 @@ class CharDecoder
 {
 	public static void main(String[] args)
 	{
-		char[] characters = {83, 101, 110, 100, 32, 121, 111, 117, 114, 32, 116, 101, 97, 99, 104, 101, 114, 32,
-				     97, 110, 32, 101, 109, 97, 105, 108, 32, 116, 111, 100, 97, 121};
+		char[] characters = {220, 241, 255, 245, 052, 313, 311, 052, 312, 303, 052, 312, 252, 245, 052, 205, 241, 251, 253, 243, 052, 203, 253, 302, 251, 244, 303, 301, 053};
 
 		for(int i=0; i < characters.length; i++)
 		{
-			System.out.print(characters[i]);
-
+			for(int j=characters[i].length)
 		}
-
 	}
 }
