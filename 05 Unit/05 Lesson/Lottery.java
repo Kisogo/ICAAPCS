3a3753727d0a6b4b7ccf19d53f8bd351bc359462
Ryan Stenmark <ryanpstenmark@gmail.com>
2015-12-17 21:06:12 -0600

    commit

--- 05 Unit/05 Lesson/Lottery.java
+++ 05 Unit/05 Lesson/Lottery.java
@@ -8,53 +8,50 @@ class Lottery
 		Random rng = new Random();
 		Scanner in = new Scanner(System.in);
 		
-		int i = 1;
-		int winningnumber = rng.nextInt(999);
-		String strwinningnumber = new String();
-		strwinningnumber = String.valueOf(winningnumber);
+		String winningnumber = new String();
 		
-		System.out.println("[debug] the winning number is: " + winningnumber);
+		// Generate lottery numbers
+		for(int i=0; i != 3; i++)
+		{
+			int nextRandInt = rng.nextInt(9); 
+			winningnumber.concat(Integer.toString(nextRandInt)); // concat random number to winning number
+		}
 		
-		System.out.print("Please enter your three numbers (e.g. 123): ");
+		// Get user's guess
+		System.out.println("Please enter your three numbers (e.g. 123): ");
 		String guess = new String(in.nextLine());
-			
-		if(guess.length() > 3) // did the user imput a valid length number?
+		
+		// Check user's guess
+		boolean[] truthtable = {false, false, false};
+		for(int k=1; k < guess.length(); k++)
 		{
-			System.out.println("Your guess cannot be longer than 3 characters.\nPlease try again.");
-			System.exit(0);
-		}	
-		else if(guess.equals(strwinningnumber)) // is the user's guess completely correct?
-		{
-			System.out.println("Winner: " + winningnumber);
-			System.out.println("Congratulations, both pairs matched.");
-			System.exit(0);
-		}
-		else
-		{
-			for(i=0; i < 2; i=i+1) // nope! start iterating through the user's input.
+			// Check numbers of user's guess
+			if(guess.substring(k,k+1).equals(winningnumber.substring(k,k+1)))
 			{
-				System.out.println(guess.substring(i,i+2) + "\n" + strwinningnumber.substring(i,i+2) + "\n" + (guess.length()-1));
-				
-				if(guess.substring(i,i+2).equals(strwinningnumber.substring(i,i+2))) // found a match!
-				{
-					if(i == 0) // the first pair matched.
-					{
-						System.out.println("Winner: " + winningnumber);
-						System.out.println("Congratulations, the front pair matched.");
-						System.exit(0);
-					}
-					else if(i == 1) // the last pair matched.
-					{
-						System.out.println("Winner: " + winningnumber);
-						System.out.println("Congratulations, the end pair matched.");
-						System.exit(0);
-					}	
-				}
+				truthtable[k] = true
 			}
-			System.out.println(i);
-			// for loop terminated; nothing matched!
-			System.out.println("Winner: " + winningnumber);
-			System.out.println("Sorry, no matches.");
+			
+			
+			
+			// figure out winning condition
+			if(truthtable[1] && truthtable[2] && truthtable[3] == true)
+			{
+				// user's entire guess is correct
+			}else if(truthtable[1] && truthtable[2] == true)
+			{
+				// user's first two guesses are correct
+			}
+			else if(truthtable[2] && truthtable[3] == true)
+			{
+				// user's last two guesses are correct
+			}
+			else
+			{
+			 	// none of user's guesses were correct.	
+			}
+			
+			
 		}
+			
 	}
 }
