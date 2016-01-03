052d51924b3eb7febf6f80ee794e71fb3c8f7e2a
ryan <ryanpstenmark@gmail.com>
2016-01-02 20:25:27 -0500

    commit

---
+++ 07 Unit/02 Lesson/CircleGraph.java
@@ -0,0 +1,18 @@
+class CircleGraph
+{
+	public static void main(String[] args)
+	{
+		double radius = 1.0;
+		
+		System.out.printf("%4s%.1f%n", "Points on a Circle of Radius ", radius);
+		System.out.printf("%4s%8s%12s%8s%n", "x1", "y1", "x2", "y2");
+		System.out.println("----------------------------------");
+		
+		
+		for(int i=1; i != 360; i++)
+		{
+			
+		}
+	
+	}
+}
