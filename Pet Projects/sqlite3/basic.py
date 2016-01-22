4c1374da018800a0c1643673cdd5a05ca2ea8ddd
root <Ryan Stenmark>
2016-01-22 15:48:23 -0600

    commit

--- Pet Projects/sqlite3/basic.py
+++ Pet Projects/sqlite3/basic.py
@@ -22,13 +22,23 @@ c.execute(''' CREATE TABLE stocks
                     # real : A floating point value.
                     # blob : A blob of data, stored exactly as it was input.
                     
+# Insert one row of data.
+c.execute("INSERT INTO stocks VALUES ('2006-01-05', 'BUY', 'RHAT', 100, 23.21)")
+
+# Insert many rows at once.
+purchases = [('2006-03-28', 'BUY', 'IBM', 1000, 45.00),
+             ('2006-04-05', 'BUY', 'MSFT', 1000, 72.00),
+             ('2006-04-06', 'SELL', 'IBM', 500, 53.00),
+            ]
+c.executemany('INSERT INTO stocks VALUES (?, ?, ?, ?, ?)', purchases)
+
 
 # Insert many rows of data.
-prices = ()
-for i in range(100):
-    prices = prices + ((random.uniform(1,10)+100,),)
+#prices = ()
+#for i in range(100):
+#    prices = prices + ((random.uniform(1,10)+100,),)
 
-c.executemany("INSERT INTO stocks VALUES ('2006-01-05', 'BUY', 'RHAT', 100, ?)", prices)
+#c.executemany("INSERT INTO stocks VALUES ('2006-01-05', 'BUY', 'RHAT', 100, ?)", prices)
 
 
 # Display a row of data (Insecurely) -- NEVER DO THIS!
@@ -37,27 +47,28 @@ c.executemany("INSERT INTO stocks VALUES ('2006-01-05', 'BUY', 'RHAT', 100, ?)",
 
 
 # Display a row of data (Securely).
-t = ('RHAT',)
-c.execute('SELECT * FROM stocks WHERE symbol=?', t)
+#t = ('RHAT',)
+#c.execute('SELECT * FROM stocks WHERE symbol=?', t)
 
+#t = ('BUY',)
+#c.execute('SELECT * FROM stocks WHERE trans=?', t)
+
+
+# Display any row of data that match REGEX
+    # '%' is wildcard character
+c.execute('SELECT * FROM stocks WHERE date LIKE "%%%%-04-%%"')
 
 # Fetch one row.
 #print(c.fetchone())
 
 
 # Fetch all rows that meet the cursor's query.
-#print(c.fetchall())
+print(c.fetchall())
 
 
 # Save (commit) the changes through the Connection object.
 conn.commit()
 
-while True:
-    try:
-        pass
-    except KeyboardInterrupt:
-        conn.close()
-        sys.exit()
 
 # Close the connection. Any uncommitted changes will be lost!
 conn.close()
